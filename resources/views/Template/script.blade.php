a870511b09a8715746684575a00be333916353ab
adesug <adesugiantoro89@gmail.com>
2021-05-09 23:26:07 +0700

    create auth

---
+++ resources/views/Template/script.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="{{asset('Admin_LTE/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('Admin_LTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('Admin_LTE/dist/js/adminlte.min.js')}}"></script>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/login',function () {
+    return view('Pengguna.login');
+})->name('login');
+
+Route::post('/postlogin','LoginController@postlogin')->name('postlogin');
+Route::get('/logout','LoginController@logout')->name('logout');
+
+Route::group(['middleware' => ['auth']], function (){
+    Route::get('/beranda','BerandaController@index');
+});
+
+
